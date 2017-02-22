5de385ab22ac8754bd3edc7dcf7174d317197ac7
hezhipeng <hezhipeng@baidu.com>
2017-02-22 18:19:10 +0800

    mv pusher_client to server_test

--- server/server_test.go
+++ server/server_test.go
@@ -1,13 +1,44 @@
 package server
 
 import (
-	"testing"
+	"bytes"
+	"fmt"
+
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/msbu-tech/go-pconn/msg"
 )
 
-func Test_StartPconnSrv(t *testing.T) {
-	// if e := StartPconnSrv(); e != nil { //try a unit test on function
-	// 	t.Error("start server failed...") // 如果不是如预期的那么就报错
-	// } else {
-	// 	t.Log("testing passed") //记录一些你期望记录的信息
-	// }
+var (
+	push_url = "http://127.0.0.1:8078/push"
+	push_req = msg.PushMsgReq{
+		Push_type:  0,
+		Channel_id: "baiduyiliaoshiyebu",
+		Device_ids: []string{"81", "7fbd15f1-f8e6-11e6-ae3f-a45e60c0c79d"},
+		Content:    "Hello World",
+	}
+)
+
+func StartPusherSrvTest() {
+	push_req_json, err := json.Marshal(push_req)
+	if err != nil {
+		panic(err)
+	}
+	req, err := http.NewRequest("POST", push_url, bytes.NewBuffer(push_req_json))
+	//req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 }
